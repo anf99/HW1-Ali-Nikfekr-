f984b46749fbac128c650bf604dc0aa73b1c333f
Ali <1a9n9f9@gmail.com>
2019-04-04 02:20:27 +0430

    exercise2

---
+++ date_and_time/date_and_time/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace date_and_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime ta =DateTime.Today;
+            Console.WriteLine(ta.Date);
+            Console.ReadKey();
+        }
+    }
+}
